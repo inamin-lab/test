4adc2c060ff0ccacd08a3af4f00d6137c0f5d532
negr <negr@julivi.com>
2020-10-09 11:43:22 +0300

    next commit 1

--- test/_c.cpp
+++ test/_c.cpp
@@ -3,5 +3,9 @@
 int main()
 {
   printf("Git repository test!\n");
+  printf("Git repository init!\n");
+  printf("Git repository add!\n");
+  printf("Git repository create branch!\n");
+  printf("Git repository commit!\n");
   for(int i=0; i<10; i++) printf("hello %f\n",2.1);
 }
